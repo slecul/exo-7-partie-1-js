9fffd98df9ab6bc371024f3cfdd6333dd8e6f297
slecul <slecul@laposte.net>
2019-01-24 16:16:54 +0100

    après correction

--- exo7js.js
+++ exo7js.js
@@ -1,6 +1,19 @@
-function valeur() {
-  var pointure = document.getElementById('firstNumber').value;
-  var naissance = document.getElementById('secondNumber').value;
-  var result = (((pointure*2)+5)*50-naissance)+1766;
-  alert(result);
+//Déclaration de la fonction.
+function weirdcalcul() {
+  // On récupère les valeurs entrées par l'utilisateur.
+  var shoeSize = document.getElementById('shoeSize').value;
+  var yearOfBirth = document.getElementById('yearOfBirth').value;
+  //Si les entrées ne sont pas des nombres, on préviens l'utilisateur.
+  if (isNaN(shoeSize) && isNaN(yearOfBirth)){
+    alert('Vous n\'avez pas entré de nombres');
+    //Sinon, on effectue le calcul chelou.
+  }else{
+    //MAIS avant de faire le calcul, on vérifie que les entrées sont cohérentes avec un humain normalement constitué: pointure entre 34 et 55 ET date de naissance entre 1900 et 2019.
+    if((shoeSize <= 34 || shoeSize >= 55) || (yearOfBirth <= 1900 || yearOfBirth >= 2019)){
+      alert('Les entrées ne correspondent pas a un adulte vivant');
+    // ENFIN , si TOUTES les conditions sont remplies, on fait le fâmeux calcul.
+    }else{
+    alert(((shoeSize*2+5)*50-yearOfBirth)+1766);
+    }
+  }
 }
